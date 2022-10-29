4d273c1a685f2b619329b76e7d3358939b096def
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2022-10-29 09:15:45 +0600

    Add files via upload

---
+++ SelectionSort/SelectionSort/src/CodeWithoutInpuut.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class CodeWithoutInpuut {
+    public static void main(String[] args) {
+        int a[]={4,3,2,7,8,-1};
+        int tem;
+        for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]>a[j]){
+                    tem=a[i];
+                    a[i]=a[j];
+                    a[j]=tem;
+                }
+            }
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+       // System.out.println(Arrays.toString(a));
+
+
+    }
+    
+}
